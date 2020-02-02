a88c3d7d50214b7f7ec1ba4067b39062f9b41e1e
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2020-02-02 13:57:55 -0500

    comments added

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,40 +1,55 @@
-
+//time complexity O (n^2)
+// best case O (n)
 public class BubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int[] sample = { 55, 3, 7, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48 };
-		int res[] = BSort(sample); 
-		System.out.println("-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <- the result should look like");
-		for (int i = 0; i < res.length; i++) {
-			System.out.printf("%d, ", res[i]);
+	/*starts by comparing the first element to next and then if the first element is bigger it is swapped. Now this element at position 2 is compared
+	 * to next element, swapped if it bigger. This continues through out the array, this makes the largest element to bubble up to the last position.
+	 * Now again we will will pass through the array again, this time to bubble up the second largest element. We will pass till n-1 position
+	 * Each pass will bubble up the respective ordered largest number to the end of array. We will pass through only remaining elements.
+	 * The moment we went through the array without a single pass, it means array is already sorted. No need of further passes
+		*/
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int[] sample = { 7, 55, 3, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48 };
+			int res[] = BSort(sample); 
+			System.out.println("-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <- the result should look like");
+			for (int i = 0; i < res.length; i++) {
+				System.out.printf("%d, ", res[i]);
+			}
 		}
-	}
-
-	private static int[] BSort(int[] sample) {
-
-		for(int j=0;j<sample.length-1;j++)
-		{
-			// flag ensure the loop breaks when there has been no swap in one iteration which indirectly means array is already sorted
-			int flag=0;
-			for(int i=0;i<sample.length-j-1;i++)
+	
+		private static int[] BSort(int[] sample) {
+	
+			for(int j=0;j<sample.length-1;j++)
 			{
-				if(sample[i]>sample[i+1])
+				// flag ensure the loop breaks when there has been no swap in one iteration which indirectly means array is already sorted
+				int flag=0;
+				for(int i=0;i<sample.length-j-1;i++)
 				{
-					int temp=sample[i];
-					sample[i]=sample[i+1];
-					sample[i+1]=temp;
-					flag=1;  // makes sure that one swap has been made
+					if(sample[i]>sample[i+1])
+					{
+						int temp=sample[i];
+						sample[i]=sample[i+1];
+						sample[i+1]=temp;
+						flag=1;  // makes sure that one swap has been made
+					}
+				}
+				
+				//to print the iterations 
+				for (int e = 0; e < sample.length; e++) {
+					System.out.printf("%d, ", sample[e]);
+				}
+				System.out.println();
+				
+				
+				if(flag==0)
+				{
+					break;
 				}
 			}
-			if(flag==0)
-			{
-				break;
-			}
+			
+			return sample;
 		}
-		
-		return sample;
+	
 	}
-
-}
+	
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,79 +1,91 @@
-
+//time complexity O (n^2)
 public class SelectionSort {
-
-	public static void main(String[] args) {
-		int[] sample = { 55, 3, 7, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48 };
-
-		//int res[]=SelectionSort(sample); // time complexity is n*n
-
-		//int res[] = SelectionSortInPlace(sample);  // time complexity is n*n
-		
-		int res[] = SelectionSortInPlace1(sample); // time complexity is n*n
-
-		System.out.println("-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <- the real sorted array");
-		for (int i = 0; i < res.length; i++) {
-			System.out.printf("%d, ", res[i]);
-		}
-
-	}
-
-	// this method is improvised version of SelectionSortInPlace
-	private static int[] SelectionSortInPlace1(int[] sample) {
-
-		for (int i = 0; i < sample.length; i++) {
-			int pmin = i; 
-			for (int j = i+1; j < sample.length; j++) {
-				if (sample[pmin] > sample[j]) {
-					pmin=j;
-				}
+	/* search for the least element in whole array, swaps it with element at 1st position. In next iteration searches for the least element from
+	 * position 2 to n and the least element is swapped with position 2. This continues through out the array
+	 */
+		public static void main(String[] args) {
+			int[] sample = { 55, 3, 7, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48 };
+	
+			System.out.println("55, 3, 7, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48 <- input array ");
+			//int res[]=SelectionSort(sample); // time complexity is n*n
+	
+			//int res[] = SelectionSortInPlace(sample);  // time complexity is n*n
+			
+			int res[] = SelectionSortInPlace1(sample); // time complexity is n*n
+	
+			System.out.println("-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <- the result should look like");
+			for (int i = 0; i < res.length; i++) {
+				System.out.printf("%d, ", res[i]);
 			}
-			int temp=sample[pmin];
-			sample[pmin]=sample[i];
-			sample[i]=temp;
+	
 		}
-		return sample;
+	
+		// this method is improvised version of SelectionSortInPlace
+		private static int[] SelectionSortInPlace1(int[] sample) {
+	
+			for (int i = 0; i < sample.length; i++) {
+				int pmin = i; 
+				for (int j = i+1; j < sample.length; j++) {
+					if (sample[pmin] > sample[j]) {
+						pmin=j;
+					}
+				}
+				int temp=sample[pmin];
+				sample[pmin]=sample[i];
+				sample[i]=temp;
+				
+				
+				//to print the iterations 
+				for (int e = 0; e < sample.length; e++) {
+					System.out.printf("%d, ", sample[e]);
+				}
+				System.out.println();
+				
+			}
+			return sample;
+		}
+		
+		// this method makes changes in the same array
+		private static int[] SelectionSortInPlace(int[] sample) {
+	
+			for (int i = 0; i < sample.length; i++) {
+				int pos = 0;
+				int min = 99999; // setting it to a very high number
+				for (int j = i; j < sample.length; j++) {
+					if (min > sample[j]) {
+						min = sample[j];
+						pos = j;
+					}
+				}
+				sample[pos] = sample[i];
+				sample[i] = min;
+			}
+			return sample;
+		}
+	
+		// this method requires extra memory for storing the extra array
+		static int[] SelectionSort(int[] sample) {
+			int res[] = new int[sample.length];
+	
+			int min = 99999; // setting it to a very high number
+			for (int i = 0; i < sample.length; i++) {
+				if (min > sample[i]) {
+					min = sample[i];
+				}
+				res[0] = min;
+			}
+	
+			for (int i = 1; i < sample.length; i++) {
+				min = 99999; // setting it to a very high number
+				for (int j = 0; j < sample.length; j++) {
+					if (min > sample[j] && sample[j] > res[i - 1]) {
+						min = sample[j];
+					}
+				}
+				res[i] = min;
+			}
+			return res;
+		}
+	
 	}
 	
-	// this method makes changes in the same array
-	private static int[] SelectionSortInPlace(int[] sample) {
-
-		for (int i = 0; i < sample.length; i++) {
-			int pos = 0;
-			int min = 99999; // setting it to a very high number
-			for (int j = i; j < sample.length; j++) {
-				if (min > sample[j]) {
-					min = sample[j];
-					pos = j;
-				}
-			}
-			sample[pos] = sample[i];
-			sample[i] = min;
-		}
-		return sample;
-	}
-
-	// this method requires extra memory for storing the extra array
-	static int[] SelectionSort(int[] sample) {
-		int res[] = new int[sample.length];
-
-		int min = 99999; // setting it to a very high number
-		for (int i = 0; i < sample.length; i++) {
-			if (min > sample[i]) {
-				min = sample[i];
-			}
-			res[0] = min;
-		}
-
-		for (int i = 1; i < sample.length; i++) {
-			min = 99999; // setting it to a very high number
-			for (int j = 0; j < sample.length; j++) {
-				if (min > sample[j] && sample[j] > res[i - 1]) {
-					min = sample[j];
-				}
-			}
-			res[i] = min;
-		}
-		return res;
-	}
-
-}
