e872bed47c16849b0cf0aeb68de610c168d4a07b
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2020-02-05 18:03:46 -0500

    comments

--- MergeSort.java
+++ MergeSort.java
@@ -1,11 +1,20 @@
-//worst case time complexity O (n(log n)
+/*worst case time complexity O (n(log n)
+ * Space complexity O  (n(log n) ) - if we do not clear the memory occupied by arrays after each level for left and right
+ * Space complexity O  (n) - if we clear the memory occupied by arrays after each level for left and right
+ * Divide and conquer
+ * Recursive
+ * Stable -> Preserves the relative order of the element
+ * Not an In-place algorithm -> the extra space taken to store indices and other stuff is dependent on size of input array 
+ * 
+ */
+
 public class MergeSort
 {
 	public static void main(String[] args) {
 		
 		int[] sample = { 55, 3, 7, 3303, 443, -12, 53, 77, 123, 4, 543, 0, 213, 299, 48};
 		MSort(sample); 
-		System.out.println("-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <- the sample result should look like");
+		System.out.println("\n-12, 0, 3, 4, 7, 48, 53, 55, 77, 123, 213, 299, 443, 543, 3303 <-  the result should look like");
 		for (int i = 0; i < sample.length; i++) {
 			System.out.printf("%d, ",sample[i]);
 		}
@@ -29,13 +38,31 @@ public class MergeSort
 		{
 			rside[i-mid]=sample[i];
 		}
+		
+		//to print the iterations 
+		System.out.println("\nlside");
+		for (int e = 0; e < lside.length; e++) {
+			System.out.printf("%d, ", lside[e]);
+		}
+		System.out.println("\nrside");
+		for (int e = 0; e < rside.length; e++) {
+			System.out.printf("%d, ", rside[e]);
+		}
+		
 		MSort(lside);
 		MSort(rside);
+		
 		Merge(lside,rside,sample);
+		
+		System.out.println("\nmerged");
+		for (int e = 0; e < sample.length; e++) {
+			System.out.printf("%d, ", sample[e]);
+		}
+		
 		return;
 	}
 	
-	private static int[] Merge(int[] sample1, int[] sample2,int[] sample) {
+	private static void Merge(int[] sample1, int[] sample2,int[] sample) {
 		
 		int i=0,j=0,k=0;
 		
@@ -69,7 +96,6 @@ public class MergeSort
 				i++;
 			}	
 		}
-		return sample;
 	}
 
 }
